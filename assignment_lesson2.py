01dae374ead7ffb7761cf04e0c1a2e75d4704f03
momaina27 <monilinez27@gmail.com>
2025-08-20 09:40:00 +0300

    Add files via upload

---
+++ assignment_lesson2.py
@@ -0,0 +1,12 @@
+my_list=[10,15,20,30,40]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1,15)
+my_list.extend([50,60,70])
+del my_list[-1]
+my_list.sort()
+index=my_list.index(30)
+print(index)
+
